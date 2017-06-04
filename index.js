6682560cb29dc3ffb7a6ef99ba63208ec2b2a259
Alberto Julio Leal Rivera <betojulio@gmail.com>
2017-06-03 23:09:51 -0500

    feat: Controller for home page.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = angular.module('followersApp', ['ngRoute'])
 app.config(function($routeProvider){
     $routeProvider
         .when('/', {
+            controller: 'main_controller',
             templateUrl: 'main.html'
         })
         .when('/:github_user/followers', {
@@ -12,5 +13,25 @@ app.config(function($routeProvider){
         })
         .otherwise({
             redirectTo: "/"
+        })
+})
+
+app.controller('main_controller', function($scope, $http, $location){
+    $scope.users_list = []
+
+    $http.get('https://api.github.com/users')
+        .then(function success(response){
+            var github_users = response.data
+            angular.forEach(github_users, function(doc){
+                var url = doc.url
+                $http.get(url)
+                    .then(function success(response){
+                        $scope.users_list.push(response.data)
+                    },function error(response){
+                        $location.path('/404')
+                    })
+            })
+        },function error(response){
+            $location.path('/404')
         })
 })

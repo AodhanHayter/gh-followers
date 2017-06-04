31026f56b9a1df9df2b9e67a3d9ab9084380a854
Alberto Julio Leal Rivera <betojulio@gmail.com>
2017-06-03 21:56:45 -0500

    feat: Creating app routes

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict'
 
-const app = angular.module('followersApp', [])
+const app = angular.module('followersApp', ['ngRoute'])
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'main.html'
+        })
+        .when('/:github_user/followers', {
+            templateUrl: 'followers.html'
+        })
+        .otherwise({
+            redirectTo: "/"
+        })
+})

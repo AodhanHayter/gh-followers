ffc310130415ddc6e32e8204568ad74aac48a2d8
D. GRAYMAN <juan.gzz.salz@gmail.com>
2018-06-18 14:54:01 -0500

    showing a list of users from github

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 'use strict'
 
 const app = angular.module('followersApp', [])
+
+$(function(){
+	var url = 'https://api.github.com/users';
+
+	$.get(url, null, function(users){
+		$('#list').empty();
+
+		users.forEach(function(user){
+			$('#list').append('<li><div class="user-button" data-id-user="' + user.id + '">' + user.id + ' - ' + user.login + '</div></li>');
+		})
+	});
+});

6a622f6928445fcfbcff4551a944986231b61260
Will Alvarez <will.alvarez@gmail.com>
2018-06-19 21:31:42 -0500

    main interface, main controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,37 +12,7 @@ angular
   .module('githubStalkerApp', [
     'ngAnimate',
     'ngResource',
-    'ui.router',
     'ngSanitize'
   ])
-  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-    var mainstate = {
-      name: 'main',
-      url: '/',
-      template: 'mainComp'
-    }
-    var followersstate = {
-      name: 'followers',
-      url: '/followers/{login}',
-      template: 'followersComp'
-          }
-    $stateProvider.state(mainstate);
-  $stateProvider.state(followersstate);
-  })
-  // .config(function ($routeProvider) {
-  //   $routeProvider
-  //     .when('/', {
-  //       templateUrl: 'views/main.html',
-  //       controller: 'MainCtrl',
-  //       controllerAs: 'main'
-  //     })
-  //     .when('/about', {
-  //       templateUrl: 'views/about.html',
-  //       controller: 'AboutCtrl',
-  //       controllerAs: 'about'
-  //     })
-  //     .otherwise({
-  //       redirectTo: '/'
-  //     });
-  // });
+ 

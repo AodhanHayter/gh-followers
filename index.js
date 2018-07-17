885846029d7c10a03a9aabee5272905ee477b86e
Alex <villaalejandro91@gmail.com>
2018-07-16 23:45:41 -0500

    Show followers

--- index.js
+++ index.js
@@ -15,7 +15,25 @@ app.controller('followersCtr',
 			$scope.users = users;
 		})
 
-		
+		$scope.follows = function(id){
+			$('.insert').html('');
+			$('.modal-title').html(id + ' - followers')
+			var followers = 'https://api.github.com/users/'+id+'/followers';
+			var html = '';
+			$http.get(followers)
+				.success(function(followers){
+					//$scope.followers = followers;
+					angular.forEach(followers, function(value, key) {
+					  console.log(value);
+					  html += '<div class="col-sm-4 insert2">'+
+		  						'<img src="'+value.avatar_url+'">'+
+		  					'<h4>'+value.id+' - '+value.login+'</h4>'+
+		  					'</div>';
+					});
+				$('.insert').html(html);
+				})
+			
+		};
 
 	}
 );

cc96a9ab75f7ca0577c741e20ae805a99f5abcca
Alex <villaalejandro91@gmail.com>
2018-07-16 23:44:22 -0500

    show users

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict'
 
-const app = angular.module('followersApp', [])
+const app = angular.module('followersApp', []);
+
+$('#followers').modal('show');
+
+app.controller('followersCtr',
+	function ($scope,$http){
+
+		var users = 'https://api.github.com/users';
+		$scope.titulo = 'followers app';
+
+		$http.get(users)
+		.success(function(users){
+			$scope.users = users;
+		})
+
+		
+
+	}
+);

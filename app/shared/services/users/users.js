071f117a317221b3ab9c0e2a49438f29047aded2
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2019-03-23 20:36:48 -0700

    fix: access_token param was not sending correctly

--- app/shared/services/users/users.js
+++ app/shared/services/users/users.js
@@ -4,22 +4,27 @@ app.factory('Users',
             var fnsObject;
             var apiKey = __env.githubApiKey;
             var usersUrl = 'https://api.github.com/users';
+            var options = {
+                params: {
+                    access_token: apiKey
+                }
+            };
 
             fnsObject = {
                 getAllUsers: function () {
-                    return $http.get(usersUrl, { access_token: apiKey });
+                    return $http.get(usersUrl, options);
                 },
                 getUserDetails: function(username) {
-                    return $http.get(usersUrl+`/${username}`, { access_token: apiKey });
+                    return $http.get(usersUrl+`/${username}`, options);
                 },
                 getUserFollowers: function(username) {
-                    return $http.get(usersUrl+`/${username}/followers`, { access_token: apiKey });
+                    return $http.get(usersUrl+`/${username}/followers`, options);
                 },
                 getUserFollowings: function(username) {
-                    return $http.get(usersUrl+`/${username}/following`, { access_token: apiKey });
+                    return $http.get(usersUrl+`/${username}/following`, options);
                 },
                 getUserRepositories: function(username) {
-                    return $http.get(usersUrl+`/${username}/repos`, { access_token: apiKey });
+                    return $http.get(usersUrl+`/${username}/repos`, options);
                 }
 
             };

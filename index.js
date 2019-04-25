cbdfa8e095154b45ac77e18a80fd775d0d3dfc00
matfirst <marcos.torres@definityfirst.com>
2019-04-25 12:24:19 -0600

    FollowersApp - Followers display

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ app.controller('followersController',
 			$scope.users = users;
         })
         
+        $scope.userSelected = '';
+        $scope.followers = [];
         
+        $scope.followersList = function(id){
+            $scope.followers = [];
+			$scope.userSelected = id;
+			var apiFollowers = 'https://api.github.com/users/'+id+'/followers';
+			$http.get(apiFollowers)
+				.success(function(followers){
+					$scope.followers = followers;				
+				})
+
+        };
          
  	}
 ); 

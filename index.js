5d542f521d91135b489429cc3c44160f49e6533e
matfirst <marcos.torres@definityfirst.com>
2019-04-25 12:18:33 -0600

    FollowersApp - Users display

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict'
 
-const app = angular.module('followersApp', [])
+const app = angular.module('followersApp', []);
+
+app.controller('followersController',
+	function ($scope,$http){
+ 		var gitUsers = 'https://api.github.com/users';
+
+ 		$http.get(gitUsers)
+		.success(function(users){
+			$scope.users = users;
+        })
+        
+        
+         
+ 	}
+); 
